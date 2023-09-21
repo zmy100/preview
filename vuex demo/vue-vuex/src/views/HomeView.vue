5acd62750937c294939e20dc5dee8d99dd946f55
zmy <3588617325@qq.com>
2023-09-21 10:39:58 +0800

    vuex初探

---
+++ vuex demo/vue-vuex/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>欢迎使用vue!!!张三</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
