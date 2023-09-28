33686df4a5fea27ad34ad0243dc3cfc333ce3af9
zmy100 <zhangmengying0703@hotmail.com>
2023-09-28 21:10:26 +0800

    使用单文件组件编写 TodoList

---
+++ Vue3 基础/demo/src/components/ListItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>{{ item }}</li>
+</template>
+
+<script>
+export default {
+  name: "listItem",
+  props: {
+    item: String,
+  },
+};
+</script>
+
+<style></style>
--- Vue3 基础/vuex demo/vue-vuex/src/views/AboutView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>张三，你好，这是介绍页面！！！</h1>
-  </div>
-</template>
